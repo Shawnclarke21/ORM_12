495f7e50230a6c13a374cc5170068602128a72f2
unknown <srclarke777@gmail.com>
2024-02-24 16:55:22 -0700

    created my models within for the categories also screated the models for the separate tags products and product tags updated the routes as well

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,42 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+   id:{
+    type:DataTypes.INTEGER,
+    allowNull:false,
+    primaryKey:true,
+    autoIncrement:true,
+
+   },
+   product_name:{
+  
+    type:DataTypes.STRING,
+    allowNull:false
+
+   },
+   stock:{
+    type:DataTypes.INTEGER,
+    allowNull:false,
+    defaultValue:10,
+    validate:{
+      isNumberic:true
+    }
+
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'category'
+      }
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal:true
+      }
+
+    }
   },
   {
     sequelize,
